48e52c398c4d0cd86e6466bb0b6dca4a32c440fb
Bahaa <0xffvirus@gmail.com>
2026-07-29 21:41:23 +0800

    Initial Commit

---
+++ ScreenTranslate/ScreenTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTranslateApp.swift
+//  ScreenTranslate
+//
+//  Created by Bahaa on 15/02/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
